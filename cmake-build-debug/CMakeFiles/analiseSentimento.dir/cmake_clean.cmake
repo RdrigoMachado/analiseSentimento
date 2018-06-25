b9494dc493bd0ea673f219dcebfcb8deb3ba0abd
RdrigoMachado <r.drigomachado@gmail.com>
2018-06-25 13:39:44 -0300

    Criação do projeto usando clion

---
+++ cmake-build-debug/CMakeFiles/analiseSentimento.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/analiseSentimento.dir/main.cpp.obj"
+  "analiseSentimento.pdb"
+  "analiseSentimento.exe"
+  "analiseSentimento.exe.manifest"
+  "libanaliseSentimento.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/analiseSentimento.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
